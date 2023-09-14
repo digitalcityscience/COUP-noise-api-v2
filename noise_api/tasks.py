1121fe8909e59a3a355090d0a995dd4193f9bf96
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-09-14 22:36:49 +0200

    Add noise calc files

--- noise_api/tasks.py
+++ noise_api/tasks.py
@@ -2,18 +2,17 @@ from celery import signals
 from celery.utils.log import get_task_logger
 
 from noise_api.dependencies import cache, celery_app
+from noise_api.noise_analysis.noisemap import run_noise_calculation
 
 # from noise_api.models.calculation_input import NoiseTask
+
 
 logger = get_task_logger(__name__)
 
 
 @celery_app.task()
 def compute_task(task_def: dict) -> dict:
-    # TODO implement calculation logic
-    # del task_def["buildings"]
-    # del task_def["roads"]
-    return task_def
+    return run_noise_calculation(task_def)
 
 
 @signals.task_postrun.connect
