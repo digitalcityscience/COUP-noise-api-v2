f88884b9fb56897e67bd70da63220be7b3db113f
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-09-16 15:08:19 +0200

    Implement context manager

--- noise_api/noise_analysis/noisemap.py
+++ noise_api/noise_analysis/noisemap.py
@@ -21,6 +21,92 @@ logger = logging.getLogger(__name__)
 DB_NAME = (os.path.abspath(".") + os.sep + "mydb").replace(os.sep, "/")
 
 
+class H2DatabaseContextManager:
+    def __enter__(self):
+        self.h2_subprocess, self.psycopg2 = self.boot_h2_database_in_subprocess()
+        self.conn, self.psycopg2_cursor = self.initiate_database_connection(
+            self.psycopg2
+        )
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cleanup()
+
+    def boot_h2_database_in_subprocess(self):
+        java_command = (
+            'java -cp "bin/*:bundle/*:sys-bundle/*" org.h2.tools.Server -pg -trace'
+        )
+
+        args = shlex.split(java_command)
+        f = open("log.txt", "w+")
+        p = subprocess.Popen(args, cwd=ORBISGIS_DIR, stdout=f)
+        print("ProcessID H2-database ", p.pid)
+
+        # Allow time for database booting
+        sleep(2)
+
+        # Database process is running
+        import_tries = 0
+        while p.poll() is None:
+            try:
+                import psycopg2
+            except ImportError:
+                print("Could not connect to database.")
+                print("Trying again in 5 seconds")
+                sleep(5)
+                import_tries += 1
+            else:
+                print("Successfully imported psycopg2")
+                return p, psycopg2
+            finally:
+                if import_tries > 4:
+                    stdout, stderr = p.communicate()
+                    print("Could not import psycopg2, trouble with database process")
+                    print(stdout, stderr)
+                    p.terminate()
+
+    def initiate_database_connection(self, psycopg2):
+        # Define our connection string
+        # DB name has to be an absolute path
+        conn_string = (
+            f"host='localhost' port=5435 dbname='{DB_NAME}' user='sa' password='sa'"
+        )
+
+        # Print the connection string we will use to connect
+        print("Connecting to database\n ->%s" % (conn_string))
+
+        # Get a connection, if a connect cannot be made an exception will be raised here
+        conn = psycopg2.connect(conn_string)
+
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        cursor = conn.cursor()
+        print("Connected!\n")
+
+        # Initialize NoiseModelling functions
+        for alias_name, function_name in queries.FUNCTIONS_TO_INIT:
+            cursor.execute(
+                queries.CREATE_ALIAS.substitute(
+                    alias=alias_name,
+                    func=function_name,
+                )
+            )
+
+        cursor.execute("CALL H2GIS_SPATIAL();")
+
+        return conn, cursor
+
+    def cleanup(self):
+        # Close connections to the database
+        print("Closing cursor")
+        self.psycopg2_cursor.close()
+
+        print("Closing database connection")
+        self.conn.close()
+
+        # Terminate the database process as it constantly blocks memory
+        self.h2_subprocess.terminate()
+
+
 def get_result_path():
     cwd = os.path.dirname(os.path.abspath(__file__))
     results_folder = os.path.abspath(f"{cwd}/results/")
@@ -32,77 +118,6 @@ def get_result_path():
 
 
 ORBISGIS_DIR = Path(__file__).parent / "orbisgis_java"
-
-
-def boot_h2_database_in_subprocess():
-    java_command = (
-        'java -cp "bin/*:bundle/*:sys-bundle/*" org.h2.tools.Server -pg -trace'
-    )
-
-    args = shlex.split(java_command)
-    f = open("log.txt", "w+")
-    p = subprocess.Popen(args, cwd=ORBISGIS_DIR, stdout=f)
-    print("ProcessID H2-database ", p.pid)
-
-    # allow time for database booting
-    sleep(2)
-
-    # database process is running
-    import_tries = 0
-    while p.poll() is None:
-        try:
-            import psycopg2
-        except ImportError:
-            print("Could not connect to database.")
-            print("Trying again in 5sec")
-            sleep(5)
-            import_tries += 1
-        # successful import continue with project
-        else:
-            print("Sucessfully imported psycopg2")
-            return p, psycopg2
-        # terminate database process in case something went wrong
-        finally:
-            if import_tries > 4:
-                stdout, stderr = p.communicate()
-                print("Could not import psycopg2, trouble with database process")
-                print(stdout, stderr)
-                p.terminate()
-
-
-# invokes H2GIS functions in the database
-# returns the database cursor (psycopg2)
-def initiate_database_connection(psycopg2):
-    # TODO: invoke db from subprocess if not running
-    # Define our connection string
-    # db name has to be an absolute path
-
-    conn_string = (
-        f"host='localhost' port=5435 dbname='{DB_NAME}' user='sa' password='sa'"
-    )
-
-    # print the connection string we will use to connect
-    print("Connecting to database\n	->%s" % (conn_string))
-
-    # get a connection, if a connect cannot be made an exception will be raised here
-    conn = psycopg2.connect(conn_string)
-
-    # conn.cursor will return a cursor object, you can use this cursor to perform queries
-    cursor = conn.cursor()
-    print("Connected!\n")
-
-    # Initialize NoiseModelling functions
-    for alias_name, function_name in queries.FUNCTIONS_TO_INIT:
-        cursor.execute(
-            queries.CREATE_ALIAS.substitute(
-                alias=alias_name,
-                func=function_name,
-            )
-        )
-
-    cursor.execute("CALL H2GIS_SPATIAL();")
-
-    return conn, cursor
 
 
 def export_result_from_db_to_geojson(cursor):
@@ -166,20 +181,17 @@ def calculate_noise_result(
 
 
 def run_noise_calculation(task_def: dict):
-    # TODO use a context manager here for the subprocess
-    h2_subprocess, psycopg2 = boot_h2_database_in_subprocess()
-    conn, psycopg2_cursor = initiate_database_connection(psycopg2)
-
-    # get noise result as json
-    noise_result_geojson = calculate_noise_result(
-        psycopg2_cursor,
-        {
-            "max_speed": task_def["max_speed"],
-            "traffic_quota": task_def["traffic_quota"],
-        },
-        task_def["buildings"],
-        task_def["roads"],
-    )
+    with H2DatabaseContextManager() as h2_context:
+        # get noise result as json
+        noise_result_geojson = calculate_noise_result(
+            h2_context.psycopg2_cursor,
+            {
+                "max_speed": task_def["max_speed"],
+                "traffic_quota": task_def["traffic_quota"],
+            },
+            task_def["buildings"],
+            task_def["roads"],
+        )
 
     # TODO remove user from here and investigate how to implement clipping
     # cityPyo_user = ""
@@ -187,15 +199,15 @@ def run_noise_calculation(task_def: dict):
     # noise_result_geojson = clip_gdf_to_project_area(noise_result_geojson, cityPyo_user)
     # print("Result geojson save in ", noise_result_geojson)
 
-    # close connections to database
-    print("closing cursor")
-    psycopg2_cursor.close()
+    # # close connections to database
+    # print("closing cursor")
+    # psycopg2_cursor.close()
 
-    print("closing database connection")
-    conn.close()
+    # print("closing database connection")
+    # conn.close()
 
-    # terminate database process as it constantly blocks memory
-    h2_subprocess.terminate()
+    # # terminate database process as it constantly blocks memory
+    # h2_subprocess.terminate()
 
     # Try to make noise computation even faster
     # by adjustiong: https://github.com/Ifsttar/NoiseModelling/blob/master/noisemap-core/
