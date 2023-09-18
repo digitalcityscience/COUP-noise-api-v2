1eba290a5601e2a8b82be28fdca2b2175c379617
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-09-18 18:28:55 +0200

    Format queries

--- noise_api/noise_analysis/queries.py
+++ noise_api/noise_analysis/queries.py
@@ -26,22 +26,35 @@ FUNCTIONS_TO_INIT = [
 
 INSERT_BUILDING = Template(
     """
-        -- Insert 1 building from automated string
-        INSERT INTO buildings (the_geom) VALUES (ST_GeomFromText($building));
-        """
+    INSERT INTO buildings (the_geom) VALUES (ST_GeomFromText($building));
+"""
 )
 
 RESET_BUILDINGS_TABLE = """
-    drop table if exists buildings;
-    create table buildings ( the_geom GEOMETRY );
-    """
+    DROP TABLE IF EXISTS buildings;
+    CREATE TABLE buildings (the_geom GEOMETRY);
+"""
 
-RESET_TRI_LVL_TABLE = """drop table if exists tri_lvl; create table tri_lvl as SELECT * from BR_TriGrid((select 
-    st_expand(st_envelope(st_accum(the_geom)), 750, 750) the_geom from ROADS_SRC),'buildings','roads_src','DB_M','',
-    {max_prop_distance},{max_wall_seeking_distance},{road_with},{receiver_densification},{max_triangle_area},
-    {sound_reflection_order},{sound_diffraction_order},{wall_absorption}); """.format(
-    **settings.computation.dict()
-)
+RESET_TRI_LVL_TABLE = Template(
+    """
+    DROP TABLE IF EXISTS tri_lvl;
+    CREATE TABLE tri_lvl AS SELECT * FROM BR_TriGrid(
+        (SELECT ST_Expand(ST_Envelope(ST_Accum(the_geom)), 750, 750) the_geom FROM ROADS_SRC),
+        'buildings',
+        'roads_src',
+        'DB_M',
+        '',
+        $max_prop_distance,
+        $max_wall_seeking_distance,
+        $road_with,
+        $receiver_densification,
+        $max_triangle_area,
+        $sound_reflection_order,
+        $sound_diffraction_order,
+        $wall_absorption
+    );
+"""
+).substitute(**settings.computation.dict())
 
 RESET_ROADS_TRAFFIC_TABLE = """
     DROP TABLE IF EXISTS roads_traffic;
@@ -116,37 +129,101 @@ RESET_ROADS_DIR_TABLES = """
             FROM roads_dir_two;
 """
 
+
 RESET_TRICONTOURING_MAP = """
-    drop table if exists tricontouring_noise_map;
-    -- create table tricontouring_noise_map AS SELECT * from ST_SimplifyPreserveTopology(ST_TriangleContouring('tri_lvl','w_v1','w_v2','w_v3',31622, 100000, 316227, 1000000, 3162277, 1e+7, 31622776, 1e+20));
-    create table tricontouring_noise_map AS SELECT * from ST_TriangleContouring('tri_lvl','w_v1','w_v2','w_v3',31622, 100000, 316227, 1000000, 3162277, 1e+7, 31622776, 1e+20);
-    -- Merge adjacent triangle into polygons (multiple polygon by row, for unique isoLevel and cellId key)
-    drop table if exists multipolygon_iso;
-    create table multipolygon_iso as select ST_UNION(ST_ACCUM(the_geom)) the_geom ,idiso, CELL_ID from tricontouring_noise_map GROUP BY IDISO, CELL_ID;
-    -- Explode each row to keep only a polygon by row
-    drop table if exists simple_noise_map;
-    -- example form internet : CREATE TABLE roads2 AS SELECT id_way, ST_PRECISIONREDUCER(ST_SIMPLIFYPRESERVETOPOLOGY(THE_GEOM),0.1),1) the_geom, highway_type t FROM roads; 
-    -- ST_SimplifyPreserveTopology(geometry geomA, float tolerance);
-    -- create table simple_noise_map as select ST_SIMPLIFYPRESERVETOPOLOGY(the_geom, 2) the_geom, idiso, CELL_ID from multipolygon_iso;
-    drop table if exists contouring_noise_map;
-    -- create table CONTOURING_NOISE_MAP as select ST_Transform(ST_SETSRID(the_geom,{0}),{1}),idiso, CELL_ID from ST_Explode('simple_noise_map'); 
-    create table CONTOURING_NOISE_MAP as select ST_Transform(ST_SETSRID(the_geom,{0}),{1}),idiso, CELL_ID from ST_Explode('multipolygon_iso'); 
-    drop table multipolygon_iso;""".format(
+    DROP TABLE IF EXISTS tricontouring_noise_map;
+    CREATE TABLE tricontouring_noise_map AS SELECT * FROM ST_TriangleContouring(
+        'tri_lvl',
+        'w_v1',
+        'w_v2',
+        'w_v3',
+        31622,
+        100000,
+        316227,
+        1000000,
+        3162277,
+        1e+7,
+        31622776,
+        1e+20
+    );
+    DROP TABLE IF EXISTS multipolygon_iso;
+    CREATE TABLE multipolygon_iso AS
+        SELECT
+            ST_UNION(ST_ACCUM(the_geom)) the_geom,
+            idiso,
+            CELL_ID
+        FROM tricontouring_noise_map
+        GROUP BY
+            IDISO,
+            CELL_ID;
+    DROP TABLE IF EXISTS simple_noise_map;
+    DROP TABLE IF EXISTS contouring_noise_map;
+    CREATE TABLE contouring_noise_map AS
+        SELECT
+            ST_Transform(ST_SETSRID(the_geom, {0}), {1}),
+            idiso,
+            CELL_ID
+        FROM
+            ST_Explode('multipolygon_iso');
+    DROP TABLE multipolygon_iso;
+""".format(
     25832, 4326
 )
 
+# RESET_TRICONTOURING_MAP = """
+#     drop table if exists tricontouring_noise_map;
+#     -- create table tricontouring_noise_map AS SELECT * from ST_SimplifyPreserveTopology(ST_TriangleContouring('tri_lvl','w_v1','w_v2','w_v3',31622, 100000, 316227, 1000000, 3162277, 1e+7, 31622776, 1e+20));
+#     create table tricontouring_noise_map AS SELECT * from ST_TriangleContouring('tri_lvl','w_v1','w_v2','w_v3',31622, 100000, 316227, 1000000, 3162277, 1e+7, 31622776, 1e+20);
+#     -- Merge adjacent triangle into polygons (multiple polygon by row, for unique isoLevel and cellId key)
+#     drop table if exists multipolygon_iso;
+#     create table multipolygon_iso as select ST_UNION(ST_ACCUM(the_geom)) the_geom ,idiso, CELL_ID from tricontouring_noise_map GROUP BY IDISO, CELL_ID;
+#     -- Explode each row to keep only a polygon by row
+#     drop table if exists simple_noise_map;
+#     -- example form internet : CREATE TABLE roads2 AS SELECT id_way, ST_PRECISIONREDUCER(ST_SIMPLIFYPRESERVETOPOLOGY(THE_GEOM),0.1),1) the_geom, highway_type t FROM roads;
+#     -- ST_SimplifyPreserveTopology(geometry geomA, float tolerance);
+#     -- create table simple_noise_map as select ST_SIMPLIFYPRESERVETOPOLOGY(the_geom, 2) the_geom, idiso, CELL_ID from multipolygon_iso;
+#     drop table if exists contouring_noise_map;
+#     -- create table CONTOURING_NOISE_MAP as select ST_Transform(ST_SETSRID(the_geom,{0}),{1}),idiso, CELL_ID from ST_Explode('simple_noise_map');
+#     create table CONTOURING_NOISE_MAP as select ST_Transform(ST_SETSRID(the_geom,{0}),{1}),idiso, CELL_ID from ST_Explode('multipolygon_iso');
+#     drop table multipolygon_iso;""".format(
+#     25832, 4326
+# )
+
+# RESET_ROADS_GLOBAL_TABLE = """
+#     DROP TABLE IF EXISTS roads_src_global;
+#     CREATE TABLE roads_src_global AS SELECT the_geom,
+#     CASEWHEN(
+#         road_type = 99,
+#         BTW_EvalSource(train_speed, trains_per_hour, ground_type, has_anti_vibration),
+#         BR_EvalSource(load_speed,lightVehicleCount,heavyVehicleCount,junction_speed,max_speed,road_type,ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom),1)),ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom),2)),ST_Length(the_geom),False)
+#         ) as db_m from roads_geo_and_traffic;
+# 	"""
+
 RESET_ROADS_GLOBAL_TABLE = """
-    drop table if exists roads_src_global;
-    CREATE TABLE roads_src_global AS SELECT the_geom, 
-    CASEWHEN(
-        road_type = 99,
-        BTW_EvalSource(train_speed, trains_per_hour, ground_type, has_anti_vibration),
-        BR_EvalSource(load_speed,lightVehicleCount,heavyVehicleCount,junction_speed,max_speed,road_type,ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom),1)),ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom),2)),ST_Length(the_geom),False)
-        ) as db_m from roads_geo_and_traffic;
-	"""
+    DROP TABLE IF EXISTS roads_src_global;
+    CREATE TABLE roads_src_global AS SELECT the_geom,
+    CASE WHEN road_type = 99 THEN
+        BTW_EvalSource(train_speed, trains_per_hour, ground_type, has_anti_vibration)
+    ELSE
+        BR_EvalSource(
+            load_speed,
+            lightVehicleCount,
+            heavyVehicleCount,
+            junction_speed,
+            max_speed,
+            road_type,
+            ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom), 1)),
+            ST_Z(ST_GeometryN(ST_ToMultiPoint(the_geom), 2)),
+            ST_Length(the_geom),
+            False
+        )
+    END as db_m
+    FROM roads_geo_and_traffic;
+"""
+
 
 RESET_ROADS_SRC_TABLE = """
-    drop table if exists roads_src;
+    DROP TABLE IF EXISTS roads_src;
     CREATE TABLE roads_src AS SELECT the_geom,
     BR_SpectrumRepartition(100,1,db_m) as db_m100,
     BR_SpectrumRepartition(125,1,db_m) as db_m125,
@@ -169,6 +246,11 @@ RESET_ROADS_SRC_TABLE = """
 
 
 RESET_ROADS_GEOM_TABLE = """
-        drop table if exists roads_geom;
-        create table roads_geom ( the_geom GEOMETRY, NUM INTEGER, node_from INTEGER, node_to INTEGER, road_type INTEGER);
+        DROP TABLE IF EXISTS roads_geom;
+        CREATE TABLE roads_geom (
+            the_geom GEOMETRY,
+            NUM INTEGER,
+            node_from INTEGER,
+            node_to INTEGER,
+            road_type INTEGER);
         """
