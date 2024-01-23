6c460db036fe90a2ce7e3523b5a93afab3439017
Andre@HCU <andre.landwehr@hcu-hamburg.de>
2024-01-23 17:54:58 +0100

    feat: test traffic settings as optional params feat: Swagger docs with 2 example requests (global traffic settings on/off)

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -3,6 +3,7 @@ import time
 from pathlib import Path
 
 import pytest
+import geopandas
 
 TEST_CASES_DIR = Path(__file__).parent / "test_cases"
 
@@ -19,6 +20,8 @@ def load_test_cases(directory: Path) -> list[dict]:
                 json_data_list.append(data)
         except Exception as e:
             return f"Error reading file {file_path.name}: {e}"
+
+    print(f"test cases {json_data_list}")
 
     return json_data_list
 
@@ -39,4 +42,10 @@ def test_noise_calculation(unauthorized_api_test_client, test_case):
 
         response = client.get(f"/noise/jobs/{job_id}/results")
         result = response.json()["result"]
-        assert result == test_case["response"]
+
+        # assert result == test_case["response"]["result"]
+        print(test_case["name"])
+        gdf = geopandas.GeoDataFrame.from_features(result["geojson"]["features"])
+        assert round(gdf["value"].max(), 2) == test_case["test_stats"]["max_value"]
+        assert round(gdf["value"].mean(), 2) == test_case["test_stats"]["mean_value"]
+
